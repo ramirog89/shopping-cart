f3aa16994b834e99b2a4d9746329555a613b8ab8
Ramiro Gonzalez <rgonzalez@makingsense.com>
2021-03-25 06:27:26 -0300

    Update shopping and result image

--- shopping.js
+++ shopping.js
@@ -1,31 +1,12 @@
-/**
-Your cart should be able to:
-add and store items with their quantities
-remove items with a given quantity
-calculate a subtotal with discounts applied
-calculate a total with a given tax rate
-
-Below is a table with some sample prices and discounts
-
-Item          Unit      Special
-              Price     Price
-----------------------------------
-Avocado       $2.00    3 for $5
-Bread         $3.99
-Cheerios      $5.49     BOGO
-Dill Pickles  $4.99      30% off
-*/
-const createItem = ({ name, price }) => ({
+const createItem = (name, price) => ({
     name,
     price
 });
-const createShopping = (shopping) => (shopping);
-let cart = [];
 // Create cart items
-const avocado = createItem({ name: 'Avocado', price: 2 });
-const bread = createItem({ name: 'Bread', price: 3.99 });
-const cheerios = createItem({ name: 'Cheerios', price: 5.49 });
-const dillPickles = createItem({ name: 'Dill Pickles', price: 4.99 });
+const avocado = createItem('Avocado', 2);
+const bread = createItem('Bread', 3.99);
+const cheerios = createItem('Cheerios', 5.49);
+const dillPickles = createItem('Dill Pickles', 4.99);
 // Define special prices for any cart item
 const specialPriceMap = {
     [avocado.name]: { name: '3 for $5', calculate: (items) => {
@@ -37,35 +18,36 @@ const specialPriceMap = {
         }
     },
     [cheerios.name]: { name: 'BOGO', calculate: (items, shopping, item) => {
-            shopping.add(Object.assign(Object.assign({}, item), { price: 0 }), items.length);
+            shopping.add(Object.assign(Object.assign({}, item), { specialPrice: 'BOGO', price: 0 }), items.length);
             return shopping.calculate(items);
         } },
     [dillPickles.name]: { name: '30% off', calculate: (items, shopping, item) => {
             return item.price * items.length * 0.30;
         } },
 };
-const shoppingCart = createShopping({
+const shoppingCart = ({
+    cart: [],
     add: (item, quantity) => {
-        [...Array(quantity).keys()].forEach(() => cart.push(item));
+        [...Array(quantity).keys()].forEach(() => shoppingCart.cart.push(item));
     },
     remove: (item, quantity) => {
-        const lastItemBeforeDelete = cart.filter(shopItem => shopItem.name === item.name).length;
-        cart = cart.filter(shopItem => shopItem.name !== item.name);
+        const lastItemBeforeDelete = shoppingCart.cart.filter(shopItem => shopItem.name === item.name).length;
+        shoppingCart.cart = shoppingCart.cart.filter(shopItem => shopItem.name !== item.name);
         shoppingCart.add(item, lastItemBeforeDelete - quantity);
     },
     calculate: (bag) => {
         return bag.reduce((total, item) => total += item.price, 0);
     },
     calculateSubTotal: () => {
-        return shoppingCart.calculate(cart);
+        return shoppingCart.calculate(shoppingCart.cart);
     },
     calculateTotalWithTax: () => {
         const itemSpecialPriceList = Object.keys(specialPriceMap);
-        const itemsWithOutSpecialPrices = cart.filter(shopItem => {
+        const itemsWithOutSpecialPrices = shoppingCart.cart.filter(shopItem => {
             return !itemSpecialPriceList.includes(shopItem.name);
         });
         const total = Object.entries(specialPriceMap).reduce((total, [key, item]) => {
-            const allItems = cart.filter(cartItem => cartItem.name === key);
+            const allItems = shoppingCart.cart.filter(cartItem => cartItem.name === key);
             return total += item.calculate(allItems, shoppingCart, allItems[0]);
         }, 0);
         return total + shoppingCart.calculate(itemsWithOutSpecialPrices);
@@ -79,6 +61,6 @@ shoppingCart.add(dillPickles, 2);
 // Remove items from the shop
 // shoppingCart.remove(avocado, 2);
 // Shopping Summary
-console.table(cart);
 console.info('Shopping Subtotal: ', shoppingCart.calculateSubTotal());
 console.info('Shopping Total with Taxes: ', shoppingCart.calculateTotalWithTax());
+console.table(shoppingCart.cart);
--- shopping.ts
+++ shopping.ts
@@ -1,21 +1,3 @@
-/**
-Your cart should be able to:
-add and store items with their quantities
-remove items with a given quantity
-calculate a subtotal with discounts applied
-calculate a total with a given tax rate
-
-Below is a table with some sample prices and discounts
-
-Item          Unit      Special
-              Price     Price
-----------------------------------
-Avocado       $2.00    3 for $5
-Bread         $3.99
-Cheerios      $5.49     BOGO
-Dill Pickles  $4.99      30% off
-*/
-
 interface CartItem {
   name: string;
   price: number;
@@ -27,6 +9,7 @@ interface SpecialPrice {
 }
 
 interface Shopping {
+  cart: CartItem[];
   add: (item: CartItem, quantity: number) => void;
   remove: (item: CartItem, quantity: number) => void;
   calculate: (bag: any) => number;
@@ -34,20 +17,16 @@ interface Shopping {
   calculateTotalWithTax: () => number;
 }
 
-const createItem = ({ name, price }: CartItem): CartItem => ({
+const createItem = (name: string, price: number): CartItem => ({
   name,
   price
 });
 
-const createShopping = (shopping: Shopping): Shopping => (shopping);
-
-let cart: CartItem[] = [];
-
 // Create cart items
-const avocado = createItem({ name: 'Avocado', price: 2 });
-const bread = createItem({ name: 'Bread', price: 3.99 });
-const cheerios = createItem({ name: 'Cheerios', price: 5.49 });
-const dillPickles = createItem({ name: 'Dill Pickles', price: 4.99 });
+const avocado = createItem('Avocado', 2);
+const bread = createItem('Bread', 3.99);
+const cheerios = createItem('Cheerios', 5.49);
+const dillPickles = createItem('Dill Pickles', 4.99);
 
 // Define special prices for any cart item
 const specialPriceMap: { [key: string]: SpecialPrice } = {
@@ -60,7 +39,7 @@ const specialPriceMap: { [key: string]: SpecialPrice } = {
     }
   },
   [cheerios.name]: { name: 'BOGO', calculate: (items, shopping, item) => {
-    shopping.add({ ...item, price: 0 }, items.length);
+    shopping.add({ ...item, specialPrice: 'BOGO', price: 0 }, items.length);
     return shopping.calculate(items);
   }},
   [dillPickles.name]: { name: '30% off', calculate: (items, shopping, item) => {
@@ -68,28 +47,29 @@ const specialPriceMap: { [key: string]: SpecialPrice } = {
   }},
 };
 
-const shoppingCart = createShopping({
+const shoppingCart: Shopping = ({
+  cart: [],
   add: (item: CartItem, quantity: number) => {
-    [...Array(quantity).keys()].forEach(() => cart.push(item));
+    [...Array(quantity).keys()].forEach(() => shoppingCart.cart.push(item));
   },
   remove: (item: CartItem, quantity: number) => {
-    const lastItemBeforeDelete = cart.filter(shopItem => shopItem.name === item.name).length;
-    cart = cart.filter(shopItem => shopItem.name !== item.name);
+    const lastItemBeforeDelete = shoppingCart.cart.filter(shopItem => shopItem.name === item.name).length;
+    shoppingCart.cart = shoppingCart.cart.filter(shopItem => shopItem.name !== item.name);
     shoppingCart.add(item, lastItemBeforeDelete - quantity);
   },
   calculate: (bag): number => {
     return bag.reduce((total, item) => total += item.price, 0);
   },
   calculateSubTotal: (): number => {
-    return shoppingCart.calculate(cart);
+    return shoppingCart.calculate(shoppingCart.cart);
   },
   calculateTotalWithTax: (): number => {
     const itemSpecialPriceList = Object.keys(specialPriceMap);
-    const itemsWithOutSpecialPrices = cart.filter(shopItem => {
+    const itemsWithOutSpecialPrices = shoppingCart.cart.filter(shopItem => {
       return !itemSpecialPriceList.includes(shopItem.name)
     });
     const total = Object.entries(specialPriceMap).reduce((total, [key, item]) => {
-      const allItems = cart.filter(cartItem => cartItem.name === key);
+      const allItems = shoppingCart.cart.filter(cartItem => cartItem.name === key);
       return total += item.calculate(allItems, shoppingCart, allItems[0]);
     }, 0);
     return total + shoppingCart.calculate(itemsWithOutSpecialPrices);
@@ -106,7 +86,7 @@ shoppingCart.add(dillPickles, 2);
 // shoppingCart.remove(avocado, 2);
 
 // Shopping Summary
-console.table(cart);
-
 console.info('Shopping Subtotal: ', shoppingCart.calculateSubTotal());
 console.info('Shopping Total with Taxes: ', shoppingCart.calculateTotalWithTax());
+
+console.table(shoppingCart.cart);
